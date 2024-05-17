ab4694bc25fb44ff8822cb68b02fc7752157f54b
Forrnaks <116074369+Forrnaks@users.noreply.github.com>
2024-05-17 11:32:29 +0200

    Add files via upload

--- gnuplot_related/kernel_density.py
+++ gnuplot_related/kernel_density.py
@@ -1,18 +1,19 @@
 reset
-	max=180.0 #max value
-	min=-180.0 #min value
+	max=10.0 #max value
+	min=0.0 #min value
 	set term postscript color enhanced font ',16'#output terminal and file
-	set output "upTYR110-PHE219.ps"
+	set output "vall_p133_l255_all_updown.ps"
 	set xrange [min:max]
-        set yrange []
+        set yrange [0:2.5]
         #to put an empty boundary around the
 	#data inside an autoscaled graph.
-	set offset graph 0.05,0.05,0.05,0.0
-	set xtics min,(max-min)/5,max
-	set style fill solid 0.5 #fillstyle
+	#set offset graph 0.05,0.05,0.05,0.0
+	set xtics 2
+	set mxtics 2
+	#set style fill solid 0.5 #fillstyle
 	set tics out nomirror
 	set ylabel "Density"
-        set xlabel "Degree ({/Symbol Y})"
+        set xlabel "Distance (A)"
         set border 3
         set xtics nomirror
         set ytics nomirror
@@ -25,15 +26,15 @@ reset
         set key top right
         set xlabel font "Helvetica,25"
         set ylabel font "Helvetica,25"
-        set ytics 0.02
+        set ytics 0.5
         set mytics 2
         set xtics font "Helvetica,20"
         set ytics font "Helvetica,20"
         
-        set style line 1 lt 1 lw 5 lc rgb "#0072bd"
-        set style line 2 lt 1 lw 5 lc rgb "#d95319"
-        set style line 3 lt 1 lw 5 lc rgb "#edb120"
-        set style line 4 lt 1 lw 5 lc rgb "#7e2f8e"
+        set style line 1 lt 1 lw 5 lc rgb "#0d2ae0" #blue
+        set style line 2 lt 1 lw 5 lc rgb "#7382e6" #lightblue
+        set style line 3 lt 1 lw 5 lc rgb "#eb4407" #redish
+        set style line 4 lt 1 lw 5 lc rgb "#e37a54" #orangish
         
 	#count and plot
-        plot "rama-aupperTYR-110.xvg" u 2:(1.0/12501):(500) smooth kdensity ls 1 title "AupTYR110", "rama-aupperPHE-219.xvg" u 2:(1.0/12501):(500) smooth kdensity ls 2 title "AupPHE219", "rama-bupperTYR-110.xvg" u 2:(1.0/12501):(500) smooth kdensity ls 3 title "BupTYR110", "rama-bupperPHE-219.xvg" u 2:(1.0/12501):(500) smooth kdensity ls 4 title "BupPHE219"
+        plot "P133_L255_v+_all_up.xvg" u ($2*10):(1.0/250020) smooth kdensity ls 1 title "P133-L255 / v^+ / up-state", "P133_L255_v-_all_up.xvg" u ($2*10):(1.0/250020) smooth kdensity ls 2 title "P133-L255 / v^- / up-state", "P133_L255_v+_all_down.xvg" u ($2*10):(1.0/250020) smooth kdensity ls 3 title "P133-L255 / v^+ / down-state", "P133_L255_v-_all_down.xvg" u ($2*10):(1.0/250020) smooth kdensity ls 4 title "P133-L255 / v^- / down-state"
